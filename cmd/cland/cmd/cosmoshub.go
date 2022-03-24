7b19f017ad635b54d14820f8bdde26fda8676d47
TomL94 <langer.tom7@gmail.com>
2022-03-24 20:14:23 +0200

    simple snapshot

--- cmd/cland/cmd/cosmoshub.go
+++ cmd/cland/cmd/cosmoshub.go
@@ -3,53 +3,104 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/server"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 )
 
 const (
 	MaxCap                 = 50000000000
-	MinStaked                 = 35000000
-	TotalClanAirdropAmount = 80000000000000 
-	DefaultDenom = "uclan"
+	MinStaked              = 35000000
+	TotalClanAirdropAmount = 80000000000000
+	DefaultDenom           = "uclan"
 )
 
-type HubSnapshot struct {
-	Accounts map[string]HubSnapshotAccount `json:"accounts"`
-	TotalAtomAmount        sdk.Int `json:"total_atom_amount"`
-	TotalAllocatedClanAmount sdk.Int `json:"total_allocated_clan"`
-	TotalAccounts         uint64  `json:"total_accounts"`
+type Snapshot struct {
+	Accounts                      map[string]SnapshotAccount `json:"accounts"`
+	TotalAirdropAccounts          uint64                     `json:"total_airdrop_accounts"`
+	TotalAccountedForStakedAmount sdk.Int                    `json:"total_accounted_for_amount"`
+	TotalStakedAmount             sdk.Int                    `json:"total_staked_amount"`
+	TotalClanAllocation           sdk.Int                    `json:"total_allocated_clan"`
 }
 
-// HubSnapshotAccount provide fields of snapshot per account
-type HubSnapshotAccount struct {
-	CosmosHubAddress       string `json:"cosmoshub_address"`
-	ClanBalance sdk.Int `json:"clan_balance"`
-	AtomOwnershipPercent sdk.Dec `json:"atom_ownership_percent"`
-	AtomStakedBalance   sdk.Int `json:"atom_staked_balance"`
-	AtomStakedBalanceForAirdrop   sdk.Int `json:"atom_staked_balance_for_airdrop"`
+// SnapshotAccount provide fields of snapshot per account
+type SnapshotAccount struct {
+	Address                 string  `json:"cosmoshub_address"`
+	StakedBalance           sdk.Int `json:"staked_balance"`
+	StakedForAirdropBalance sdk.Int `json:"staked_for_airdrop"`
+	AirdropOwnershipPercent sdk.Dec `json:"staked_ownership_percent"`
 }
 
-type Account struct {
-	Address       string `json:"address,omitempty"`
-	AccountNumber uint64 `json:"account_number,omitempty"`
-	Sequence      uint64 `json:"sequence,omitempty"`
-}
+//type Account struct {
+//	Address       string `json:"address,omitempty"`
+//	AccountNumber uint64 `json:"account_number,omitempty"`
+//	Sequence      uint64 `json:"sequence,omitempty"`
+//}
 
+//func SnapshotToClaimRecordsCmd() *cobra.Command {
+//	cmd := &cobra.Command{
+//		Use:   "snapshot-to-claim-records [input-snapshot-file] [output-claim-records-json]",
+//		Short: "Transform a Cosmos Hub snapshot to a Claim module records",
+//		Long: `Transform a Cosmos Hub snapshot to Claim records in a format suitable for the Claim module genesis state
+//Example:
+//	cland snapshot-to-claim-records snapshot.json claim-records.json
+//`,
+//
+//		RunE: func(cmd *cobra.Command, args []string) error {
+//			snapshotFile := args[0]
+//			snapshotJSON, err := ioutil.ReadFile(snapshotFile)
+//			if err != nil {
+//				return err
+//			}
+//
+//			var snapshot Snapshot
+//			err = json.Unmarshal(snapshotJSON, &snapshot)
+//			if err != nil {
+//				return err
+//			}
+//
+//			claimRecords := claimRecordsFromSnapshot(snapshot)
+//			claimRecordsJSON, err := json.MarshalIndent(claimRecords, "", "    ")
+//			if err != nil {
+//				return err
+//			}
+//			outputFile := args[1]
+//			err = ioutil.WriteFile(outputFile, claimRecordsJSON, 0600)
+//
+//			return err
+//		}}
+//	return cmd
+//}
+//
+//func claimRecordsFromSnapshot(snapshot Snapshot) []claimtypes.ClaimRecord {
+//	claimRecords := make([]claimtypes.ClaimRecord, snapshot.TotalAirdropAccounts)
+//
+//	i := 0
+//	for _, acc := range snapshot.Accounts {
+//		claimRecords[i] = claimtypes.ClaimRecord{
+//			Address: acc.Address,
+//			InitialClaimableAmount: sdk.Coins{sdk.Coin{
+//				Denom:  DefaultDenom,
+//				Amount: acc.ClanBalance,
+//			}},
+//			ActionCompleted: []bool{false, false, false, false, false},
+//		}
+//
+//		i++
+//	}
+//
+//	return claimRecords
+//}
 
-func ExportHubSnapshotCmd() *cobra.Command {
+func ExportSnapshotCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "export-hub-snapshot [input-genesis-file] [output-snapshot-json]",
+		Use:   "export-hub-snapshot [input-genesis-file] [clan-allocation] [output-snapshot-json]",
+		Args:  cobra.ExactArgs(3),
 		Short: "Export snapshot from a provided Cosmos Hub genesis export",
 		Long: `Export snapshot from a provided Cosmos Hub genesis export
 Example:
@@ -59,36 +110,30 @@ Example:
 		RunE: func(cmd *cobra.Command, args []string) error {
 
 			clientCtx := client.GetClientContextFromCmd(cmd)
-
 			serverCtx := server.GetServerContextFromCmd(cmd)
 			config := serverCtx.Config
-
 			config.SetRoot(clientCtx.HomeDir)
 
 			genesisFile := args[0]
-			snapshotOutput := args[1]
+			clanAllocation := args[1]
+			outputFile := args[2]
 
-			// Read genesis file
-			genesisJSON, err := os.Open(genesisFile)
-			if err != nil {
-				return err
+			var snapshot Snapshot
+			clanAllocationInt, ok := sdk.NewIntFromString(clanAllocation)
+			if !ok {
+				return fmt.Errorf("failed to parse clan-allocation to number: %s", clanAllocation)
 			}
-			defer genesisJSON.Close()
+			snapshot.TotalClanAllocation = clanAllocationInt
 
-			var snapshot HubSnapshot
-			snapshot.Accounts = make(map[string]HubSnapshotAccount)
-			snapshot.TotalAllocatedClanAmount = sdk.ZeroInt()
-
-			snapshot = exportSnapShotFromGenesisFile(clientCtx, genesisFile, "uatom",snapshot)
-			// export snapshot json
+			snapshot = exportSnapshotFromGenesisFile(clientCtx, genesisFile, snapshot)
 			snapshotJSON, err := json.MarshalIndent(snapshot, "", "    ")
 			if err != nil {
 				return fmt.Errorf("failed to marshal snapshot: %w", err)
 			}
 
-			err = ioutil.WriteFile(snapshotOutput, snapshotJSON, 0600)
+			err = ioutil.WriteFile(outputFile, snapshotJSON, 0600)
 			return err
-			
+
 		},
 	}
 
@@ -97,140 +142,74 @@ Example:
 }
 
 // compare balance with max cap
-func getMin(balance sdk.Dec) sdk.Dec {
-	if balance.GTE(sdk.NewDec(MaxCap)) {
-		return sdk.NewDec(MaxCap)
-	} else {
-		return balance
+func accountedForBalance(balance sdk.Int) sdk.Int {
+	if balance.LT(sdk.NewInt(MinStaked)) {
+		return sdk.ZeroInt()
 	}
+
+	return sdk.MinInt(balance, sdk.NewInt(MaxCap))
 }
 
-
-func getDenominator(snapshotAccs	map[string]HubSnapshotAccount) sdk.Int {
+func getDenominator(snapshotAccs map[string]SnapshotAccount) sdk.Int {
 	denominator := sdk.ZeroInt()
 	for _, acc := range snapshotAccs {
-		curAccBalance := acc.AtomStakedBalance.ToDec()
-
-		if curAccBalance.LT(sdk.NewDec(MinStaked)) {
-			continue
-		}
-
-		denominator = denominator.Add(getMin(curAccBalance).RoundInt())
+		denominator = denominator.Add(accountedForBalance(acc.StakedBalance))
 
 	}
 	return denominator
 }
 
-
-func exportSnapShotFromGenesisFile(clientCtx client.Context, genesisFile string, denom string, snapshot HubSnapshot) HubSnapshot {
+func exportSnapshotFromGenesisFile(clientCtx client.Context, genesisFile string, snapshot Snapshot) Snapshot {
 	appState, _, _ := genutiltypes.GenesisStateFromGenFile(genesisFile)
 	stakingGenState := stakingtypes.GetGenesisStateFromAppState(clientCtx.Codec, appState)
-	authGenState := authtypes.GetGenesisStateFromAppState(clientCtx.Codec, appState)
 
-	snapshotAccs := make(map[string]HubSnapshotAccount)
-	for _, account := range authGenState.GetAccounts() {
-		if account.TypeUrl == "/cosmos.auth.v1beta1.BaseAccount" {
-			_, ok := account.GetCachedValue().(authtypes.GenesisAccount)
-			if ok {
-				var byteAccounts []byte
-
-				byteAccounts, err := codec.NewLegacyAmino().MarshalJSON(account.GetCachedValue())
-				if err != nil {
-					fmt.Printf("No account found for bank balance %s \n", string(byteAccounts))
-					fmt.Println(err.Error())
-					continue
-				}
-				var accountAfter Account
-				if err := codec.NewLegacyAmino().UnmarshalJSON(byteAccounts, &accountAfter); err != nil {
-					continue
-				}
-
-				snapshotAccs[accountAfter.Address] = HubSnapshotAccount{
-					CosmosHubAddress:         accountAfter.Address,
-					AtomStakedBalance:   sdk.ZeroInt(),
-				}
-			}
-		}
-	}
-
-
-	// Make a map from validator operator address to the v42 validator type
+	snapshotAccs := make(map[string]SnapshotAccount)
 	validators := make(map[string]stakingtypes.Validator)
 	for _, validator := range stakingGenState.Validators {
 		validators[validator.OperatorAddress] = validator
 	}
 
+	totalStaked := sdk.ZeroInt()
 	for _, delegation := range stakingGenState.Delegations {
 		address := delegation.DelegatorAddress
 
 		acc, ok := snapshotAccs[address]
 		if !ok {
-			fmt.Printf("No account found for delegation address %s \n", address)
-			continue
+			acc = SnapshotAccount{
+				Address:                 address,
+				StakedBalance:           sdk.ZeroInt(),
+				StakedForAirdropBalance: sdk.ZeroInt(),
+				AirdropOwnershipPercent: sdk.ZeroDec(),
+			}
 		}
 
 		val := validators[delegation.ValidatorAddress]
-		stakedAtoms := delegation.Shares.MulInt(val.Tokens).Quo(val.DelegatorShares).RoundInt()
-		acc.AtomStakedBalance = acc.AtomStakedBalance.Add(stakedAtoms)
-
+		staked := delegation.Shares.MulInt(val.Tokens).Quo(val.DelegatorShares).RoundInt()
+		acc.StakedBalance = acc.StakedBalance.Add(staked)
 		snapshotAccs[address] = acc
+
+		totalStaked = totalStaked.Add(staked)
 	}
 
-	denominator := getDenominator(snapshotAccs)
-	totalBalance := sdk.ZeroInt()
-	totalAtomBalance := sdk.NewInt(0)
-	totalAccounts := 0
-	for address, acc := range snapshotAccs {
-		currAccBalance := acc.AtomStakedBalance.ToDec()
-		currAccBalanceMin := getMin(currAccBalance)
+	totalAccountedForAmount := getDenominator(snapshotAccs)
+	totalAirdropAccounts := 0
+	for _, acc := range snapshotAccs {
+		currAccBalance := acc.StakedBalance
+		curAccountedFor := accountedForBalance(currAccBalance).ToDec()
+		acc.StakedForAirdropBalance = curAccountedFor.RoundInt()
+		acc.AirdropOwnershipPercent = curAccountedFor.QuoInt(totalAccountedForAmount)
 
-		if denominator.IsZero() {
-			acc.AtomOwnershipPercent = sdk.NewInt(0).ToDec()
-		} else {
-			acc.AtomOwnershipPercent = currAccBalanceMin.QuoInt(denominator)
-			
+		if curAccountedFor.GT(sdk.ZeroDec()) {
+			totalAirdropAccounts = totalAirdropAccounts + 1
 		}
 
-		if currAccBalance.IsZero() {
-			acc.ClanBalance = sdk.ZeroInt()
-			snapshotAccs[address] = acc
-			continue
-		}
-
-		if currAccBalance.LT(sdk.NewDec(MinStaked)) {
-			acc.ClanBalance = sdk.ZeroInt()
-			snapshotAccs[address] = acc
-			continue
-		}
-	
-		acc.ClanBalance = acc.AtomOwnershipPercent.MulInt(sdk.NewInt(TotalClanAirdropAmount)).RoundInt()
-		totalBalance = totalBalance.Add(acc.ClanBalance)
-		totalAtomBalance = totalAtomBalance.Add(acc.AtomStakedBalance)
-		totalAccounts = totalAccounts + 1
-		snapshotAccount, exists := snapshot.Accounts[address]
-
-		if !exists {
-			snapshot.Accounts[address] = acc
-		} else {
-			fmt.Printf("No account exists")
-			if snapshotAccount.ClanBalance.IsNil() {
-				snapshotAccount.ClanBalance = sdk.ZeroInt()
-			}
-
-			snapshotAccount.ClanBalance = snapshotAccount.ClanBalance.Add(acc.ClanBalance)
-			snapshotAccount.AtomStakedBalance = snapshotAccount.AtomStakedBalance.Add(acc.AtomStakedBalance)
-			snapshot.Accounts[address] = snapshotAccount
-		}
+		snapshotAccs[acc.Address] = acc
 	}
 
-	snapshot.TotalAtomAmount = totalAtomBalance
-	snapshot.TotalAllocatedClanAmount = snapshot.TotalAllocatedClanAmount.Add(totalBalance)
-	snapshot.TotalAccounts = uint64(totalAccounts)
+	snapshot.Accounts = snapshotAccs
+	snapshot.TotalAirdropAccounts = uint64(totalAirdropAccounts)
+	snapshot.TotalAccountedForStakedAmount = totalAccountedForAmount
+	snapshot.TotalStakedAmount = totalStaked
 
-	fmt.Printf("Complete read genesis file %s \n", genesisFile)
-	fmt.Printf("# accounts: %d\n", totalAccounts)
-	fmt.Printf("total atom supply read from genesis: %s\n", totalAtomBalance.String())
-	fmt.Printf("total clan allocated in airdrop: %s\n", totalBalance.String())
 	return snapshot
 }
-
