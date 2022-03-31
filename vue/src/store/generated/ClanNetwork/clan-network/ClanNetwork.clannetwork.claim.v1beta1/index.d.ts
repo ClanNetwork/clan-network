795feac89f470514e607554fa189e76ea0bfa295
AmitKendler <amitkendler1@gmail.com>
2022-03-31 15:59:15 +0300

    remove vue files + change config

--- vue/src/store/generated/ClanNetwork/clan-network/ClanNetwork.clannetwork.claim.v1beta1/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ClaimEthRecord } from "./module/types/clan/claim/v1beta1/claim_eth_record";
-import { ClaimRecord } from "./module/types/clan/claim/v1beta1/claim_record";
-import { Params } from "./module/types/clan/claim/v1beta1/params";
-export { ClaimEthRecord, ClaimRecord, Params };
-declare const _default;
-export default _default;
